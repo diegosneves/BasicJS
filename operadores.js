a943a2044d5d545450dd388ac557d1f6eac2ac0d
Dusksins <neves.diegoalex@outlook.com>
2021-01-12 13:59:34 -0300

    chore: First Commit

---
+++ operadores.js
@@ -0,0 +1,9 @@
+//Crie uma variável mesmoNome que verifica se nomeDoFulano é igual a nomeDoBeltrano;
+
+function verificarNome(nome1, nome2){
+    return (nome1 === nome2);
+}
+
+const mesmoNome = verificarNome("Fulano", "Fulano");
+
+console.log(mesmoNome);

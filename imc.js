46397e7c0c3765a35667325ca3a51861557232ad
Dusksins <neves.diegoalex@outlook.com>
2021-01-14 13:00:03 -0300

    Chore: Implementado a funciton retornarClassificacao(imc)

--- imc.js
+++ imc.js
@@ -16,6 +16,20 @@
      {tipo: "Obesidade grave", grau: 3}
  ];
 
- 
+ function retornarClassificacao(resultadoImc){
+     let classificacaoAtual = "";
+     if(resultadoImc < 18.5){
+        classificacaoAtual = "Magreza";
+     } else if(resultadoImc >= 18.5 && resultadoImc <= 24.99){
+         classificacaoAtual = "Normal";
+     } else if(resultadoImc >= 25 && resultadoImc <= 29.99){
+         classificacaoAtual = "Sobrepeso";
+     } else if(resultadoImc >= 30 && resultadoImc <= 39.99){
+         classificacaoAtual = "Obesidade";
+     } else {
+         classificacaoAtual = "Obesidade grave";
+     }
+     return classificacaoAtual;
+ }
 
  console.log(imc);

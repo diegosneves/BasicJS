2c2f60da1bb5fb37c25d4e9665d65ffd0d13e15a
Dusksins <neves.diegoalex@outlook.com>
2021-01-14 14:51:20 -0300

    Chore: Inserido as functions (exibirResultado e mensagemOms) e refatorado a function (retornarClassificacao)

--- imc.js
+++ imc.js
@@ -3,8 +3,8 @@
  * como referência para saber a saúde de uma pessoa com base no seu peso e altura.
  */
 
- const peso = 102;
- const altura = 1.88;
+ const peso = 80;
+ const altura = 1.98;
 
  const imc = (peso / altura ** 2).toFixed(2);
 
@@ -19,17 +19,33 @@
  function retornarClassificacao(resultadoImc){
      let classificacaoAtual = "";
      if(resultadoImc < 18.5){
-        classificacaoAtual = "Magreza";
+        classificacaoAtual = classificacao[0];
      } else if(resultadoImc >= 18.5 && resultadoImc <= 24.99){
-         classificacaoAtual = "Normal";
+         classificacaoAtual = classificacao[1];
      } else if(resultadoImc >= 25 && resultadoImc <= 29.99){
-         classificacaoAtual = "Sobrepeso";
+         classificacaoAtual = classificacao[2];
      } else if(resultadoImc >= 30 && resultadoImc <= 39.99){
-         classificacaoAtual = "Obesidade";
+         classificacaoAtual = classificacao[3];
      } else {
-         classificacaoAtual = "Obesidade grave";
+         classificacaoAtual = classificacao[4];
      }
      return classificacaoAtual;
  }
 
- console.log(imc);
+ function mensagemOms(grauDeObesidade){
+     if(grauDeObesidade == 3){
+         console.log("\nÉ importante procurar um médico para avaliar sua saúde");
+     } else if(grauDeObesidade > 0 && grauDeObesidade < 3){
+         console.log("\nCuidado! Você está acima do peso recomendado pela OMS.");
+     } else {
+         return;
+     }
+ }
+
+ function exibirResultado(imc, resultadoDaClassificacao){
+     const topo = "|     IMC     |      Classificação       |    Grau de Obesidade     |\n";
+     console.log(`${topo}|   ${imc}     |       ${resultadoDaClassificacao.tipo}                     ${resultadoDaClassificacao.grau}             `);
+     mensagemOms(resultadoDaClassificacao.grau);
+ }
+
+exibirResultado(imc, retornarClassificacao(imc));

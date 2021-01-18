635718908aca29d88730a43cc9b9b93caa194de8
Dusksins <neves.diegoalex@outlook.com>
2021-01-18 18:30:27 -0300

    Final: Codigo finalizado!

--- operacoesBancarias.js
+++ operacoesBancarias.js
@@ -12,29 +12,42 @@ const usuario = {
     saldo: 0
 };
 
+//Um array de trasacoes do tipo "credito" para utilizar como teste.
 const transacaoDoTipoCredito = [
-    {tipo: "credito", valor: 50.5},
-    {tipo: "credito", valor: 1500.5},
-    {tipo: "credito", valor: 505.5}
+    {tipo: "credito", valor: 50},
+    {tipo: "credito", valor: 120}
 ];
 
+//Um array de trasacoes do tipo "debito" para utilizar como teste.
 const transacaoDoTipoDebito = [
-    {tipo: "debito", valor: 50.5},
-    {tipo: "debito", valor: 520.5},
-    {tipo: "debito", valor: 20}
+    {tipo: "debito", valor: 80},
+    {tipo: "debito", valor: 30}
 ];
 
-function criarTransacao(transacao){
-    usuario.transacao.push(transacao);
-    usuario.saldo = transacao.tipo == "credito" ? usuario.saldo + transacao.valor : usuario.saldo + transacao.valor;
+/**
+ * Função que insere uma transação no atributo(array) do objeto usuario.
+ * @param {*} novaTransacao recebe um objeto(transacao) como parâmetro.
+ */
+function criarTransacao(novaTransacao){
+    //Insere o objeto na ultima posicao do array(atributo transacao do usuario).
+    usuario.transacao.push(novaTransacao);
+
+    //Atualiza o saldo do usuario com base no tipo de transacao recebida como parametro.
+    //Foi utilizado um operador ternario para simplificar o codigo.
+    usuario.saldo = novaTransacao.tipo === "credito" ? usuario.saldo + novaTransacao.valor : usuario.saldo - novaTransacao.valor;;
 }
 
-function getTransacaoMaisAltaPorTipo(tipo){ // Tipo = credito/debito
+/**
+ * Função que retorna a transacao com o valor mais alto, registrado.
+ * @param tipo Recebe um tipo de transacao (credito ou debito).
+ * @retuns Um objeto de um array cujo o valor da transacao seja o maior dentro do array.
+ */
+function getTransacaoMaisAltaPorTipo(tipo){
     let maiorValor = 0;
     let altaTrasacao = usuario.transacao[0];
 
     for (let i = 0; i < usuario.transacao.length; i++) {
-        if(usuario.transacao[i].tipo == tipo){ // Valida os tipo de transacoes.
+        if(usuario.transacao[i].tipo === tipo){ // Valida os tipo de transacoes.
             if(usuario.transacao[i].valor > maiorValor){
                 maiorValor = usuario.transacao[i].valor;
                 altaTrasacao = usuario.transacao[i];
@@ -43,14 +56,60 @@ function getTransacaoMaisAltaPorTipo(tipo){ // Tipo = credito/debito
     }
     return altaTrasacao;
 }
+/**
+ * Função que retorna a média dos valores de todas a transações dentro do atributo transacao(array) do usuário.
+ * @returns Retorna a média dos valores.
+ */
+function getMediaDosValoresDasTransacoes(){
+    let valorMedio = 0;
+    let somaValores = 0;
+    for (let i = 0; i < usuario.transacao.length; i++) {
+        somaValores += usuario.transacao[i].valor;        
+    }
+    valorMedio = (somaValores / usuario.transacao.length).toFixed(2);
+    return valorMedio;
+}
 
+/**
+ * Função que retorna o número de transações de cada tipo e o retorno da função deve ser um objeto.
+ * @returns Retorna o objeto com os dados.
+ */
+function getContadorDeTransacoes(){
+    //Objeto contador com os atributos zerados.
+    const contador = {
+        credito: 0,
+        debito: 0
+    }
+
+    //Laço que percorre o array(atributo transacao do objeto usuario), verificando o tipo de transação e somando mais um ao contador.
+    for (let i = 0; i < usuario.transacao.length; i++) {
+        //Novamente usado o operador ternario para facilitar o código.
+        usuario.transacao[i].tipo === "credito" ? contador.credito++ : contador.debito++;        
+    }
+    return contador; //Retorna o objeto com os atributos atualizados.
+}
+
+/* Testar as functions */
+
+//Laco de repeticao usado para facilitar a insercao dos arrays de apoio(testes).
 for (let i = 0; i < transacaoDoTipoCredito.length; i++) {
     criarTransacao(transacaoDoTipoCredito[i]);
 }
 
+//Laco de repeticao usado para facilitar a insercao dos arrays de apoio(testes).
 for (let i = 0; i < transacaoDoTipoDebito.length; i++) {
     criarTransacao(transacaoDoTipoDebito[i]);
 }
 
-console.log(getTransacaoMaisAltaPorTipo("credito"));
-console.log(getTransacaoMaisAltaPorTipo("debito"));
+const creditoMaisAlto = getTransacaoMaisAltaPorTipo("credito");
+const debitoMaisAlto = getTransacaoMaisAltaPorTipo("debito");
+
+
+console.log(`Transação tipo: ${creditoMaisAlto.tipo} | Valor: ${creditoMaisAlto.valor}`);
+console.log(`Transação tipo: ${debitoMaisAlto.tipo} | Valor: ${debitoMaisAlto.valor}`);
+
+console.log(`Saldo atual do Usuário: ${usuario.saldo}`);
+
+console.log(`Valor médio das Transações: ${getMediaDosValoresDasTransacoes()}`)
+
+console.log(`Total de Transações:\nCrédito - ${getContadorDeTransacoes().credito} | Débito: ${getContadorDeTransacoes().debito}`);
